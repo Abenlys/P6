afe71883fd13d96176970491dd506aa4c98c217c
Thomas Ramillon <zerglavisse@gmail.com>
2023-05-11 11:31:50 +0200

    tentative de creation des works

--- FrontEnd/scripts/index.js
+++ FrontEnd/scripts/index.js
@@ -227,6 +227,12 @@ async function init() {
     miseEnFormeButtonEtFilterGallery()
     modeEdition()
     manageModal()
+    // rattacher la fonction sendNewWork à Publier les changements
+    const publish = document.querySelector('.bulle')
+    publish.addEventListener('click', function () {
+        sendNewWork(formData)
+      })
+    
     
 }
 init()
--- FrontEnd/scripts/modal.js
+++ FrontEnd/scripts/modal.js
@@ -1,4 +1,16 @@
+const titre = document.querySelector('#titre')
+const category = document.querySelector('#category')
+const validPicture = document.querySelector('.add-picture-valid')
+const imgByDefault = document.querySelector(".fa-image")
+const imagePreview = document.getElementById("preview")
+const inputValueImg = document.getElementById("file-upload")
+const gestionDisplay = document.querySelector('.gestion-display')
+const arrowLeft = document.querySelector('.fa-arrow-left')
+// const publish = document.querySelector('.bulle')
+let formData
+// let works = []
 let trashIndex
+
 // Fonction delete pour supprimer un objet de l'API
 async function deleteObjects(trashIndex) {
     try {
@@ -20,15 +32,10 @@ async function deleteObjects(trashIndex) {
                 
             }
         })
-        console.log(trashIndex)
-        console.log(response)
-        console.log(`${baseUrl}works/${trashIndex}`)
-        console.log(`Bearer ${token}`)
         if (!response.ok) {
             throw new Error(`la suppression de l'objet a échoué`)
         }
         const result = await response.json()
-        console.log(result)
     } catch (error) {
         console.error(`une erreur est survenue :`, error.message)
     }
@@ -54,6 +61,154 @@ function getIndexFromTrash() {
     } 
 }
 
+// Prévisualisation des images que l'on veut ajouter aux backend + test format + test poids
+function getPicture() {
+    let file = inputValueImg.files[0]
+    console.log(file)
+    if (file) {
+        const regex = /\.(png|jpe?g)$/i
+        if (regex.test(file.name)) {
+            if (file.size > 4 * 1024 * 1024) {
+                alert(`l'image est trop volumineuse`)
+            } else {
+                let fileReader = new FileReader()
+                fileReader.onload = function (event) {
+                    imagePreview.setAttribute('src', event.target.result)
+                    if (imagePreview.getAttribute('src') !== null) {
+                    imagePreview.style.display = "block"
+                    gestionDisplay.style.display = "none"
+                    }
+                }
+                fileReader.readAsDataURL(file)
+                suggestTitle()
+                }
+        } else {
+            alert("le fichier doit être au format png, jpg ou jpeg")
+        }
+    }
+}
+
+
+// Retraitement de la chaine de caractère de file.name
+function suggestTitle() {
+let str = inputValueImg.files[0].name
+let leftPart = str.substring(0, str.indexOf("."))
+let leftPartWithoutHyphen = leftPart.replace(/-/g, ' ')
+capitalizeFirstLetter(leftPartWithoutHyphen)
+titre.value = finalString
+}
+
+function capitalizeFirstLetter(string) {
+    return finalString = string.charAt(0).toUpperCase() + string.slice(1)
+}
+
+
+// Vérification de l'alimentation des champs
+inputValueImg.addEventListener("input", checkform)
+titre.addEventListener("input", checkform)
+category.addEventListener("input", checkform)
+
+function checkform() {
+    if (inputValueImg.value !== "" && titre.value.length > 4 && category.value !== "") {
+        validPicture.style.backgroundColor = "#1D6154"
+        return true
+    } else {
+        validPicture.style.backgroundColor = "#A7A7A7"
+        return false
+    }
+}
+
+
+// On teste que tous les champs sont correctement rempli, la fonction doit renvoyé true
+function fieldRegularity() {
+    if (checkform() == false) {
+        alert("les champs du formulaire sont mal remplis")
+        return false
+    } else {
+        return true
+    }
+}
+
+// creation de l'objet formData
+function createObject() {
+    imageForPost = inputValueImg.value
+    titleForPost = titre.value
+    categoryForPost = category.value
+    const newObject = {'image': imageForPost, 'title': titleForPost, 'category': categoryForPost}
+    const formData = new FormData()
+    formData.append('id', 0)
+    formData.append('title', newObject.title)
+    formData.append('image', newObject.image)
+    formData.append('category', newObject.category)
+    formData.append('userId', 0)
+    return formData
+}
+
+// stockage des objets dans l'array works, push dans works
+validPicture.addEventListener('click', function (e) {
+    e.preventDefault()
+    if (fieldRegularity() === true ) {
+        const formData = createObject()
+        console.log(formData)
+    }
+})
+
+//requete post de l'array works
+async function sendNewWork(formData) {
+    try {
+        const data = JSON.parse(localStorage.getItem('data'))
+        console.log(data)
+        let token
+        if (data) {
+            token = data.token
+        }
+        console.log(token)
+        if (!token) {
+            throw new Error('Aucun token trouvé dans le localStorage')
+        }
+        const response = await fetch(`${baseUrl}works`, {
+            method: 'POST',
+            headers: {
+                'content-Type' : 'multipart/form-data',
+                'accept' : `application/json`,
+                'Authorization': `Bearer ${token}` 
+            },
+            body: formData
+        })
+        if (!response.ok) {
+            throw new Error(`la création de l'objet a échoué`)
+        }
+        const result = await response.json()
+    } catch (error) {
+        console.error(`une erreur est survenue :`, error.message)
+    }
+    document.querySelector('.modal-gallery').innerHTML = ""
+    const data = await communiquer()
+    recupImageTitle(data, ".modal-gallery")
+    suppressionHtmlGallery()
+    recupImageTitle(data, ".gallery")
+    // works = []
+}
+
+
+// Gestion du comportement de arrow left de la modale 2
+
+arrowLeft.addEventListener('click', function () {
+    if (imagePreview.getAttribute('src') !== "" || titre.value !== "" || category.value !== "" ) {
+        imagePreview.setAttribute('src', "")
+        imagePreview.style.display = "none"
+        gestionDisplay.style.display = "flex"
+        inputValueImg.value = ""
+        titre.value = ""
+        category.value = ""
+        validPicture.style.backgroundColor = "#A7A7A7"
+    } else if (imagePreview.getAttribute('src') === "") {
+        document.querySelector('#modal2').style.display = "none"
+        document.querySelector('.modal-premier').style.display = "block"
+
+    }
+})
+
 
 
 function manageModal() {
@@ -74,6 +229,16 @@ function manageModal() {
         modal.addEventListener('click', closeModal)
         modal.querySelector('.js-modal-close').addEventListener('click', closeModal)
         modal.querySelector('.js-modal-stop').addEventListener('click', stopPropagation)
+        modal.querySelector('.add-picture').addEventListener('click', function (e) {
+            modal.querySelector('.modal-premier').style.display = "none"
+            modal.querySelector('#modal2').style.display = "block"
+            modal.querySelector('#modal2').addEventListener('click', (e) => {
+                e.stopPropagation()
+            })
+        })
+        const crossModal2 = document.querySelector('#modal2').querySelector('.fa-xmark')
+        crossModal2.addEventListener('click', closeModal)
+
         document.querySelector('.modal-gallery').innerHTML = ""
         const data = await communiquer()
         recupImageTitle(data, ".modal-gallery")
@@ -87,6 +252,8 @@ function manageModal() {
         }
         e.preventDefault()
         window.setTimeout(function () {
+            modal.querySelector('.modal-premier').style.display = 'block'
+            modal.querySelector('#modal2').style.display = 'none'
             modal.style.display = "none"
             modal = null
         }, 400)
@@ -95,8 +262,7 @@ function manageModal() {
         modal.removeEventListener('click', closeModal)
         modal.querySelector('.js-modal-close').removeEventListener('click', closeModal)
         modal.querySelector('.js-modal-stop').removeEventListener('click', stopPropagation)
-        
-       
+ 
     }
 
     const stopPropagation = function (e) {
@@ -135,3 +301,8 @@ function manageModal() {
     })
     
 }
+
+
+
+
+
